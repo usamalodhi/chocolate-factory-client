81559a17f6434e24fe1865cb4b2c15e9c404952d
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-18 09:54:33 +0100

    Add components index.tsx file which functions as centralised file to export all components from

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import HeadComponent from './HeadComponent/HeadComponent';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
+import ReviewsGrid from './ReviewsGrid/ReviewsGrid';
+
+export { HeadComponent, Header, Footer, FeaturedProducts, ReviewsGrid };
