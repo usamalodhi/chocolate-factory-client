5ff5650177a19041097a0fee6c72c2ac7246e36d
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-18 09:56:05 +0100

    Add HeadComponent - meta data for each page

---
+++ src/components/HeadComponent/HeadComponent.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { HeadProps } from './HeadInterface';
+
+const HeadComponent = ({ contentTitle, contentDescription }: HeadProps) => {
+  return (
+    <Head>
+      <meta property='og:title' content={contentTitle} />
+      <meta property='og:description' content={contentDescription} />
+      <meta property='og:url' content='https://example.com' />
+      <meta property='og:type' content='website' /> <link rel='icon' href='/favicon.ico' />
+    </Head>
+  );
+};
+
+export default HeadComponent;
