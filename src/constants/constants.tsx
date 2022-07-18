d44291c9582208b8910d1f4b943e8330cd15a0cc
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-07-18 09:55:33 +0100

    Add constants file - contains constants and mockData which will move to CMS and Server during development

---
+++ src/constants/constants.tsx
@@ -0,0 +1,21 @@
+//mock-data => will get from server
+const TESTIMONIALS = [
+  {
+    name: 'Brandon P.',
+    review: 'The best desserts I have ever had in my life!',
+  },
+  {
+    name: 'Krysta B.',
+    review: 'The best desserts I have ever had in my life!',
+  },
+  {
+    name: 'Darcy L.',
+    review: 'The best desserts I have ever had in my life!',
+  },
+  {
+    name: 'Daniel T.',
+    review: 'The best desserts I have ever had in my life!',
+  },
+];
+
+export { TESTIMONIALS };
